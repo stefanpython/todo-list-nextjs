4814fb4ad54731a95009a1285931f9f06d685e0f
stefanpython <stefanhtml@yahoo.com>
2024-03-25 17:35:11 +0200

    Update todos complete status in the db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 import { prisma } from "@/db";
+import { TodoItem } from "@/components/TodoItem";
 
 function getTodos() {
   return prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
 }
 
 export default async function Home() {
@@ -22,7 +29,7 @@ export default async function Home() {
 
       <ul className="pl-4">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
